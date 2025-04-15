ce235f2358df26c8c3dcf9fc015f5eee25bc7861
SamuelCIPOLAT <samuel.cipolat@oclock.school>
2025-04-15 14:04:01 +0200

    Debug route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import LogIn from './pages/LogIn';
+import Profile from './pages/Profile';
+import SearchPage from './pages/SearchPage';
+import SignIn from './pages/SignIn';
 
 export default function App() {
   return (
     <>
-      {/* En-tête du site */}
-      <Header />
-      {/* Contenu principal */}
-      <HomePage />
-      {/* Pied de page */}
-      <Footer />
+      <BrowserRouter>
+        {/* En-tête du site */}
+        <Header />
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/recettes" element={<SearchPage />} />
+            <Route path="/connexion" element={<LogIn />} />
+            <Route path="/inscription" element={<SignIn />} />
+            <Route path="/profil" element={<Profile />} />
+            {/* <Route path='' element={}/> */}
+            <Route
+              path="*"
+              element={<div>Erreur 404 : cette page n'a pas été trouvée !</div>}
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App.tsx';
 
-import HomePage from './pages/HomePage.tsx';
-import LogIn from './pages/LogIn.tsx';
-import Profile from './pages/Profile.tsx';
-import SearchPage from './pages/SearchPage.tsx';
-import SignIn from './pages/SignIn.tsx';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      {/* Rendu de l'app (Home Page) */}
-      <App />
-      {/* Import qui servira à gérer les routes  */}
-      <Routes>
-        {/* <Route /> Pour chaque element (ex: page connexion / page recette / page recherche...*/}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recettes" element={<SearchPage />} />
-        <Route path="/connexion" element={<LogIn />} />
-        <Route path="/inscription" element={<SignIn />} />
-        <Route path="/profil" element={<Profile />} />
-        {/* <Route path='' element={}/> */}
-        <Route
-          path="*"
-          element={<div>Erreur 404 : cette page n'a pas été trouvée !</div>}
-        />
-      </Routes>
-    </BrowserRouter>
+    {/* Rendu de l'app (Home Page) */}
+    <App />
   </StrictMode>,
 );
