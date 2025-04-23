b8dd472fe8ba543a74413df67ac44197aff137ae
EtienneMrqs <etienne.c.marquis@gmail.com>
2025-04-23 17:44:45 +0200

    Add base api url in env and declare global in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
+declare global {
+  interface Window {
+    apiBaseUrl: string | undefined;
+  }
+}
+
+window.apiBaseUrl = import.meta.env.API_BASE_URL;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
