96ec34afa5bd4dfac74cadb8bca184d087066fb8
EtienneMrqs <etienne.c.marquis@gmail.com>
2025-04-24 13:35:48 +0200

    Comment declare global: doesnt work

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
-declare global {
-  interface Window {
-    apiBaseUrl: string | undefined;
-  }
-}
+// TODO : Changer la méthode d'appel de la variable .env
+// (cause des bugs)
+// declare global {
+//   interface Window {
+//     apiBaseUrl: string | undefined;
+//   }
+// }
 
-window.apiBaseUrl = import.meta.env.API_BASE_URL;
+// window.apiBaseUrl = import.meta.env.API_BASE_URL;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
