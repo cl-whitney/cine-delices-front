229aadd4f19852a9cf7d31f2a7ff172d598e305c
cl-whitney <wh.claire31@gmail.com>
2025-08-24 01:37:52 +0200

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  // IMPORTANT : nom EXACT de ton repo GitHub
-  base: '/cine-delices-front/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  
+  base: '/cine-delices-front/',  
+  build: { outDir: 'dist' },
 });
