43b85d53280d22975fb5178bf095a244230adb61
cl-whitney <wh.claire31@gmail.com>
2025-08-24 01:20:56 +0200

    Deploy trying

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  // IMPORTANT : nom EXACT de ton repo GitHub
+  base: '/cine-delices-front/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  
 });
