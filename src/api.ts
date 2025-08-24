35c27d88e00edfadd82fa6b7817b12fcfb8deca4
cl-whitney <wh.claire31@gmail.com>
2025-08-24 02:34:14 +0200

    test deploy

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3000/api' });
+const api = axios.create({
+  baseURL: "https://cine-delices-api.onrender.com/api", // ton vrai domaine Render
+});
 
 export default api;
